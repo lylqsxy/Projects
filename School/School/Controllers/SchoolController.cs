4188cba2c5191ece9d1575abc994d54b8d152a07
Nicky LI <nickyqi@gmail.com>
2016-08-11 20:57:29 +1200

    Evening Session 11/8

---
+++ School/School/Controllers/SchoolController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using School.Models;
+
+namespace School.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult Index()
+        {
+            var db = new StudentManagementEntities();
+            var list = db.Subjects.ToList();
+            return View(list);
+        }
+
+        public ActionResult CreateStudent()
+        {
+
+            return View();
+        }
+    }
+}
