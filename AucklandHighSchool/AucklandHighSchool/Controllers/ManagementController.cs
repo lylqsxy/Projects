7a917ec4104411fbb7694b13326c4e9ba20e74b1
Nicky LI <nickyqi@gmail.com>
2016-08-18 20:52:22 +1200

    8.18

---
+++ AucklandHighSchool/AucklandHighSchool/Controllers/ManagementController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AucklandHighSchool.Models.ViewModel;
+
+namespace AucklandHighSchool.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult SubjectList()
+        {
+            using (AucklandHighSchoolEntities db  = new AucklandHighSchoolEntities())
+            {
+                return View(db.Subjects.Include("Classes").ToList());
+            }
+            
+        }
+
+        public ActionResult TeacherList()
+        {
+            using (AucklandHighSchoolEntities db = new AucklandHighSchoolEntities())
+            {
+                List<TeacherList> tl = new List<TeacherList>();
+                db.Teachers.Include("Classes").ToList().ForEach(x => tl.Add( new TeacherList { Name = x.FirstName + " " + x.LastName, Gender = x.Gender }));
+                return View(tl);
+            }
+        }
+
+        public ActionResult StudentList()
+        {
+            using(AucklandHighSchoolEntities db = new AucklandHighSchoolEntities())
+            {
+                List<StudentList> sl = new List<StudentList>();
+                db.Students.Include("Enrollments").ToList().ForEach(x => sl.Add(new StudentList { Name = x.FirstName + " " + x.LastName, Gender = x.Gender}));
+                return View(sl);
+            }
+        }
+
+        public ActionResult ClassList()
+        {
+            using (AucklandHighSchoolEntities db = new AucklandHighSchoolEntities())
+            {
+                return View(db.Classes.Include("Subject").Include("Teacher").ToList());
+            }
+        }
+    }
+}
---
+++ AucklandHighSchool/AucklandHighSchool/Models/ViewModel/StudentList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AucklandHighSchool.Models.ViewModel
+{
+    public class StudentList
+    {
+        public String Name { get; set; }
+        public String Gender { get; set; }
+    }
+}
