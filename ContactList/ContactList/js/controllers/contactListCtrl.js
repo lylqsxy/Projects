8bca56ecebc55d238b19cf889cce5b08f6b85601
Nicky LI <nickyqi@gmail.com>
2017-09-23 22:12:09 +1200

    Test

--- ContactList/ContactList/js/controllers/contactListCtrl.js
+++ ContactList/ContactList/js/controllers/contactListCtrl.js
@@ -8,12 +8,21 @@
 
 contactListApp.controller('contactListCtrl', function ($scope, $http, $filter) {
 
+    $scope.showTable = false;
+    $scope.showLoading = true;
+    $scope.showList = false;
+    $scope.propertyName = 'id';
+    $scope.reverse = false;
+    $scope.arrow = $scope.reverse ? '▼' : '▲';
+
     var getContactList = function () {
         $http({
             method: "Get",
             url: "http://jsonplaceholder.typicode.com/users"
         }).then(function SentOk(result) {
             $scope.contactList = result.data;
+            $scope.showTable = true;
+            $scope.showLoading = false;
         }, function Error(result) {
             console.log(result);
         });
@@ -21,7 +30,14 @@ contactListApp.controller('contactListCtrl', function ($scope, $http, $filter) {
 
     getContactList();
 
-    $scope.showDetails = function (a) {
-        console.log(a)
+    $scope.showDetails = function (data) {
+        $scope.$broadcast('showMsgBox', data);
+        angular.element('#msgbox').modal('show');
+    }
+
+    $scope.sortBy = function (propertyName) {
+        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+        $scope.propertyName = propertyName;
+        $scope.arrow = $scope.reverse ? '▼' : '▲';
     }
 })
---
+++ ContactList/ContactList/js/directives/msgBox.js
@@ -0,0 +1,24 @@
+﻿/* 
+   Name        : msgbox.js
+   Version     : 1.0.0
+   Author      : Nicky
+   Date        : 22-09-2017 
+   Description : This Angular JS directive file hold functions for Manage msgbox.html file
+                 The file is used for common MsgBox.
+*/
+
+contactListApp.directive('cMsgbox', function () {
+    return {
+        restrict: 'E',
+        templateUrl: './views/msgbox.html',
+        controller: ['$scope', function ($scope) {
+            $scope.$on('showMsgBox', function (event, data) {
+                $scope.msgboxData = data;
+                $scope.okFun = function () {
+                    $scope.$emit('msgboxDone', data);
+                };
+            });
+        }]
+
+    };
+});
